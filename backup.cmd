d42aa65d6044a6e8b0a16251566d6b624ab613ec
Volodymyr Hoida <devizer@users.github.com>
2017-11-01 19:43:36 +0300

    Source code folder renamed to src

---
+++ backup.cmd
@@ -0,0 +1,8 @@
+pushd "%LOCALAPPDATA%"
+echo [System.DateTime]::Now.ToString("yyyy-MM-dd,HH-mm-ss") | powershell -command - > .backup.timestamp
+for /f %%i in (.backup.timestamp) do set datetime=%%i
+popd
+
+"C:\Program Files\7-Zip\7zG.exe" a -t7z -mx=9 -mfb=128 -md=128m -ms=on -xr!.git -xr!bin -xr!obj -xr!packages ^
+  "C:\Users\Backups on Google Drive\Universe.SqlServerJam (%datetime%).7z" .
+
