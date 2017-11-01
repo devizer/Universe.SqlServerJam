ef34715265c09d8b71bb71c31d6cd793751aeebe
Volodymyr Hoida <devizer@users.github.com>
2017-11-01 19:24:22 +0300

    Fixed Build-and-Test.cmd

--- Universe.SqlServerJam/Modern-Build-and-Test.cmd
+++ Universe.SqlServerJam/Modern-Build-and-Test.cmd
@@ -1,16 +1,11 @@
 rem *** BUILD ***
 call Prepare-Nuget-and-Build-Tools.cmd
 "%BUILD_TOOLS_ONLINE%\nuget.exe" restore
-"%MS_BUILD_2017%" CacheInvalidation.sln /t:Rebuild /v:m /p:Configuration=Debug
-"%MS_BUILD_2017%" CacheInvalidation.sln /t:Rebuild /v:m /p:Configuration=Release
-
-rem *** START REDIS ***
-pushd SampleDb.Tests\redis-2.8
-call restart.cmd
-popd
+"%MS_BUILD_2017%"  /t:Rebuild /v:m /p:Configuration=Debug
+"%MS_BUILD_2017%"  /t:Rebuild /v:m /p:Configuration=Release
 
 rem *** TEST *** 
-pushd SampleDb.Tests\bin\Debug\
-"%NUNIT_CONSOLE_RUNNER%" --labels=On --workers=1 SampleDb.Tests.exe
+pushd Universe.SqlServerJam.Tests\bin\Release
+"%NUNIT_CONSOLE_RUNNER%" --labels=On --workers=1 Universe.SqlServerJam.Tests.dll
 "%REPORT_UNIT%" .\ 1>report_uit.log 2>&1
 popd
