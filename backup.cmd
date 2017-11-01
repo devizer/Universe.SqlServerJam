3a59261b18edae3b55f070ec55b535eb585752a7
Volodymyr Hoida <devizer@users.github.com>
2017-11-01 19:47:32 +0300

    Fixed backup.cmd

--- backup.cmd
+++ backup.cmd
@@ -3,6 +3,6 @@ echo [System.DateTime]::Now.ToString("yyyy-MM-dd,HH-mm-ss") | powershell -comman
 for /f %%i in (.backup.timestamp) do set datetime=%%i
 popd
 
-"C:\Program Files\7-Zip\7zG.exe" a -t7z -mx=9 -mfb=128 -md=128m -ms=on -xr!.git -xr!bin -xr!obj -xr!packages ^
+"C:\Program Files\7-Zip\7zG.exe" a -t7z -mx=9 -mfb=128 -md=128m -ms=on -xr!.git -xr!bin -xr!obj -xr!packages -xr!.vs ^
   "C:\Users\Backups on Google Drive\Universe.SqlServerJam (%datetime%).7z" .
 
