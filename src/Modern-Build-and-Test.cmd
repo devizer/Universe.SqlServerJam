29fb6458e11452b27b5a1579ea2d4cc8e74dad15
devizer <devizer@users.github.com>
2018-05-13 20:49:58 +0300

    updated Discovery of local build tools

--- src/Modern-Build-and-Test.cmd
+++ src/Modern-Build-and-Test.cmd
@@ -1,14 +1,16 @@
+type Prepare-NuGet-And-build-tools.ps1 | powershell -c -
+call ~local-build-tools.cmd
+
 rem *** BUILD ***
-call Prepare-Nuget-and-Build-Tools.cmd
-"%BUILD_TOOLS_ONLINE%\nuget.exe" restore
-"%MS_BUILD_2017%"  /t:Rebuild /v:m /p:Configuration=Debug
-"%MS_BUILD_2017%"  /t:Rebuild /v:m /p:Configuration=Release
+"%NUGET_EXE%" restore
+"%MSBUILD_EXE%" /t:Rebuild /v:m /p:Configuration=Debug
+"%MSBUILD_EXE%" /t:Rebuild /v:m /p:Configuration=Release
 
 rem *** TEST *** 
 if not defined TEST_SQL_NET_DURATION_OF_Ping set TEST_SQL_NET_DURATION_OF_Ping=3000
 if not defined TEST_SQL_NET_DURATION_OF_Upload set TEST_SQL_NET_DURATION_OF_Upload=7000
 if not defined TEST_SQL_NET_DURATION_OF_Download set TEST_SQL_NET_DURATION_OF_Download=7000
 pushd Universe.SqlServerJam.Tests\bin\Release
-"%NUNIT_CONSOLE_RUNNER%" --labels=On --workers=1 Universe.SqlServerJam.Tests.dll | tee tests.log
-"%REPORT_UNIT%" .\ 1>report_uit.log 2>&1
+"%NUNIT_RUNNER_EXE%" --labels=On --workers=1 Universe.SqlServerJam.Tests.dll | tee tests.log
+"%REPORT_UNIT_EXE%" .\ 1>report_uit.log 2>&1
 popd
---
+++ src/Prepare-NuGet-and-Build-Tools.ps1
@@ -0,0 +1,96 @@
+$packages = @("vswhere", "ReportUnit",
+              "NUnit.ConsoleRunner", "NUnit.Extension.NUnitV2Driver", "NUnit.Extension.NUnitV2ResultWriter", 
+              "xunit.runner.console", "xunit");
+
+# Arrange
+$baseDir=$Env:LocalAppData; if (!$baseDir) { $baseDir=$Env:AppData; }
+$workDir = "$baseDir\temp\build-tools-online.temp"
+Write-Host "Build Tools Folder: $workDir" -ForegroundColor DarkGray
+$packagesDir = "$workDir\packages"
+New-Item -ItemType Directory -Path $packagesDir -EA SilentlyContinue | out-null
+$color="Magenta"
+[System.Net.ServicePointManager]::ServerCertificateValidationCallback={$true}; $webClient=new-object System.Net.WebClient; 
+$report_Cmd = "~local-build-tools.cmd"
+[IO.File]::WriteAllText($report_Cmd, "@REM Local Build Tools. Auto-generated.`r`n");
+
+function AddVar { param([string]$var, [string]$value)
+  [IO.File]::AppendAllText($report_Cmd, "set $var=$value`r`n");
+  # $Env:$var=$value
+  # [Environment]::SetEnvironmentVariable($var, $value, "User")
+}
+
+function GetSHA1 { param([string]$fileName)
+  $sha1 = New-Object System.Security.Cryptography.SHA1CryptoServiceProvider 
+  [System.BitConverter]::ToString( $sha1.ComputeHash([System.IO.File]::ReadAllBytes($fileName))).Replace("-","")
+}
+
+function SmartDownload { param([string]$key, [string]$url, [string]$fileName)
+  $path = ""; if (Test-Path "$workdir\${key}.path") { $path = Get-Content "$workdir\${key}.path"; }
+  $sha1 = ""; if (Test-Path "$workdir\${key}.sha1") { $sha1 = Get-Content "$workdir\${key}.sha1"; }
+  if ($path -And (Test-Path $path) -And ($sha1 -eq (GetSHA1 $path))) { 
+    Write-Host "$fileName already present: $path" -ForegroundColor DarkGray;
+    return $path;
+  }
+  $path = "$workdir\$fileName";
+  Write-Host "Caching '$url' as`r`n        [$path]" -ForegroundColor $color
+  $webClient.DownloadFile($url, $path);
+  $path > "$workdir\${key}.path"
+  GetSHA1 $path > "$workdir\${key}.sha1"
+  return $path
+}
+
+function SmartNugetInstall { param([string]$package) 
+  $done=""; if (Test-Path "$packagesDir\$package.is-done") { $done = Get-Content "$packagesDir\$package.is-done"; }
+  if (-Not $done) {
+    pushd "$packagesDir"
+    Write-Host  "Caching nuget package [$package]" -ForegroundColor $color
+    & "$nuget" install $package >> ..\nuget.log 2>&1
+    if ($LastExitCode -ne 0) {
+      Write-Host "Unabale to install $package using nuget" -ForegroundColor Red
+      popd; return;
+    }
+    "Shure" > "$workDir\packages\$package.is-done"
+    popd
+  }
+  else {
+    Write-Host "Package $package already cached" -ForegroundColor DarkGray;
+  }  
+  return "$workDir\packages"
+}
+
+# Download nuget.exe
+$nuget = SmartDownload "NuGet(cache)" "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe" "NuGet.exe"
+AddVar "NUGET_EXE" $nuget
+
+# Downloading packes
+foreach ($p in $packages) { $p_Path = SmartNugetInstall $p; }
+
+# VS 2017 and later
+$vswhere = join-Path -Path "$packagesDir\vswhere*" -ChildPath "tools\vswhere.exe" -Resolve
+if ($vswhere) { 
+  Write-Host "vswhere: $vswhere" -ForegroundColor DarkGray
+} else {
+  Write-Host "Unable to find vswhere.exe. Check is internet connection works and support TLS/SSL" -ForegroundColor Red
+}
+AddVar "VSWHERE_EXE" $vswhere
+
+$vs_Dir = (& "$vswhere" -latest -products * -requires Microsoft.Component.MSBuild -property installationPath) | Out-String;
+$vs_Dir = "$vs_Dir".Trim([char]10,[char]13)
+AddVar "VS_PATH" "$vs_Dir"
+if ($vs_Dir) { 
+  $msbuild_x86 = join-Path -Path "$vs_Dir" -ChildPath "MSBuild\*\Bin\MSBuild.exe" -Resolve
+  $msbuild_x64 = join-Path -Path "$vs_Dir" -ChildPath "\MSBuild\*\Bin\amd64\MSBuild.exe" -Resolve
+  $msbuild=$msbuild_x64; if (-Not $msbuild) { $msbuild=$msbuild_x86; }
+}
+AddVar "MSBUILD_EXE" $msbuild
+AddVar "MSBUILD_x86_EXE" $msbuild_x86
+AddVar "MSBUILD_x64_EXE" $msbuild_x64
+
+$nunit_Runner = join-Path -Path "$packagesDir\NUnit.ConsoleRunner.*" -ChildPath "tools\nunit*-console.exe" -Resolve
+AddVar "NUNIT_RUNNER_EXE" $nunit_Runner
+
+$xunit_Runner = join-Path -Path "$packagesDir\xunit.runner.console.*" -ChildPath "tools\net4*\xunit.console.exe" -Resolve
+AddVar "XUNIT_RUNNER_EXE" $xunit_Runner
+
+$report_Exe = join-Path -Path "$packagesDir\ReportUnit.*" -ChildPath "tools\ReportUnit*.exe" -Resolve
+AddVar "REPORT_UNIT_EXE" $report_Exe
