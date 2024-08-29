b4528a68501a990c0ab3f3a93dd56942a72b4145
devizer <devizer@users.github.io>
2024-08-29 05:19:42 +0300

    12 system-info [skip appveyor]

--- Scripts/Show-System-Info.sh
+++ Scripts/Show-System-Info.sh
@@ -1,20 +1,25 @@
 script=https://raw.githubusercontent.com/devizer/test-and-build/master/install-build-tools-bundle.sh; (wget -q -nv --no-check-certificate -O - $script 2>/dev/null || curl -ksSL $script) | bash > /dev/null
 Say "Benchmark for [$(Get-CpuName)]"
-7z b -mmt=1 -md=22
+if [[ -n "$(command -v 7zz)" ]]; then
+  7zz b -mmt=1 -md=22
+else
+  7z b -mmt=1 -md=22
+fi
 
-  if [[ "$(uname -s)" == Linux ]]; then
-     echo >/dev/null
-  elif [[ "$(uname -s)" == Darwin ]]; then
-     brew install fio
-     # install nproc
-     echo '#!/usr/bin/env bash
-           sysctl -n machdep.cpu.thread_count' | sudo tee /usr/local/bin/nproc > /dev/null
-     sudo chmod +x /usr/local/bin/nproc
-  elif [[ "$(uname -s)" == *"MINGW"* ]] || [[ "$(uname -s)" == *"MSYS"* ]]; then
-     choco install -y --no-progress fio
-  else
-    echo "Warning! Unknown OS"
-  fi
+# Fix missing fio and nproc
+if [[ "$(uname -s)" == Linux ]]; then
+   echo >/dev/null
+elif [[ "$(uname -s)" == Darwin ]]; then
+   brew install fio
+   # install nproc
+   echo '#!/usr/bin/env bash
+         sysctl -n machdep.cpu.thread_count' | sudo tee /usr/local/bin/nproc > /dev/null
+   sudo chmod +x /usr/local/bin/nproc
+elif [[ "$(uname -s)" == *"MINGW"* ]] || [[ "$(uname -s)" == *"MSYS"* ]]; then
+   choco install -y --no-progress fio
+else
+  echo "Warning! Unknown OS"
+fi
 
 Say "Disk Benchmark for [$HOME]"
 export DISABLE_UNICODE=true
