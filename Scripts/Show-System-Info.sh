e080c0196a82b42255eced75ba21d49db11eb4f0
devizer <devizer@users.github.io>
2024-08-29 05:08:17 +0300

    10 system-info [skip appveyor]

--- Scripts/Show-System-Info.sh
+++ Scripts/Show-System-Info.sh
@@ -1,17 +1,17 @@
 script=https://raw.githubusercontent.com/devizer/test-and-build/master/install-build-tools-bundle.sh; (wget -q -nv --no-check-certificate -O - $script 2>/dev/null || curl -ksSL $script) | bash > /dev/null
 Say "Benchmark for [$(Get-CpuName)]"
-echo "NPROC: $(nproc)"
 7z b -mmt=1 -md=22
 
   if [[ "$(uname -s)" == Linux ]]; then
      echo >/dev/null
   elif [[ "$(uname -s)" == Darwin ]]; then
      brew install fio
+     alias nproc="sysctl -n machdep.cpu.thread_count"
   elif [[ "$(uname -s)" == *"MINGW"* ]] || [[ "$(uname -s)" == *"MSYS"* ]]; then
      choco --version
      choco install -y --no-progress fio
   else
-    echo "Unknown OS"
+    echo "Warning! Unknown OS"
   fi
 
 
