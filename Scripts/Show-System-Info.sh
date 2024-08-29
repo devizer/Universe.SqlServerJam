2f21fc63a516917e530d2e33d5defc2b6720748a
devizer <devizer@users.github.io>
2024-08-29 04:56:22 +0300

    6th system-info [skip appveyor]

--- Scripts/Show-System-Info.sh
+++ Scripts/Show-System-Info.sh
@@ -1,9 +1,18 @@
 script=https://raw.githubusercontent.com/devizer/test-and-build/master/install-build-tools-bundle.sh; (wget -q -nv --no-check-certificate -O - $script 2>/dev/null || curl -ksSL $script) | bash > /dev/null
-Say "Hello"
-echo "CPU: $(Get-CpuName)"
-
-Say "CPU Benchmark"
+Say "Benchmark for [$(Get-CpuName)]"
 7z b -mmt=1 -md=22
 
-Say "Disk Benchmark for $HOME"
+  if [[ "$(uname -s)" == Linux ]]; then
+     echo >/dev/null
+  elif [[ "$(uname -s)" == Darwin ]]; then
+     brew install fio
+  elif [[ "$(uname -s)" == *"MINGW"* ]] || [[ "$(uname -s)" == *"MSYS"* ]]; then
+     choco --version
+     choco install -y --no-progress fio
+  else
+    echo "Unknown OS"
+  fi
+
+
+Say "Disk Benchmark for [$HOME]"
 File-IO-Benchmark 'HOME' "$HOME" 1G 20 1
