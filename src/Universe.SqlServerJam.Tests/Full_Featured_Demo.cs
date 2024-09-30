d99faaaf81a33cbd73c7a75085a1627de2d86b2a
devizer <devizer@users.github.io>
2024-09-30 14:17:02 +0300

    10 Min Server Memory [skip appveyor] ***NO_CI***

--- src/Universe.SqlServerJam.Tests/Full_Featured_Demo.cs
+++ src/Universe.SqlServerJam.Tests/Full_Featured_Demo.cs
@@ -213,7 +213,7 @@ namespace Universe.SqlServerJam.Tests
                         }
 
 
-                        if (man.IsAzure && Debugger.IsAttached) Debugger.Break();
+                        // if (man.IsAzure && Debugger.IsAttached) Debugger.Break();
                         if (!man.IsAzure)
                         {
                             var prevRecovery = dbOptions.RecoveryMode;
