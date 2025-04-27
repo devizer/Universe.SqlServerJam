e034ceb61d94fa4f51dcdda598546e8788d52e15
devizer <devizer@github.io>
2025-04-27 17:20:08 +0300

    Tests for Version Comparer

---
+++ src/Universe.SqlServerJam.Tests/NumericPermutations.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Universe.SqlServerJam.Tests
+{
+    // Narayana Pandita’s algorithm that returns indexes only
+    public static class SimplePermutations
+    {
+        public static IEnumerable<int[]> GetAll(int elementsCount) => GetAll(elementsCount, false);
+        public static IEnumerable<int[]> GetAllIsolated(int elementsCount) => GetAll(elementsCount, true);
+
+        // Zero based
+        public static IEnumerable<int[]> GetAll(int elementsCount, bool needNewCopy)
+        {
+            if (elementsCount == 0) yield break;
+            if (elementsCount == 1)
+            {
+                yield return new int[1] { 0 };
+                yield break;
+            }
+
+            int[] array = new int[elementsCount];
+            for(int i = 0; i < elementsCount; i++) array[i] = i;
+            while (NextPermutation(array))
+            {
+                if (needNewCopy)
+                {
+                    int[] copy = new int[elementsCount];
+                    for (int i = 0; i < elementsCount; i++) copy[i] = array[i];
+                    yield return copy;
+                }
+                else
+                {
+                    yield return array;
+                }
+            }
+        }
+
+#if NET46_OR_GREATER || NETCOREAPP3_1_OR_GREATER
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+#endif
+        static bool NextPermutation(int[] array)
+        {
+            int n = array.Length;
+
+            int i = n - 2;
+            while (i >= 0 && array[i] >= array[i + 1]) i--;
+            if (i < 0) return false;
+
+            int j = n - 1;
+            while (array[j] <= array[i]) j--;
+
+            Swap(array, i, j);
+            Reverse(array, i + 1, n - 1);
+
+            return true;
+        }
+
+#if NET46_OR_GREATER || NETCOREAPP3_1_OR_GREATER
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+#endif
+        static void Swap(int[] array, int i, int j)
+        {
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+
+#if NET46_OR_GREATER || NETCOREAPP3_1_OR_GREATER
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+#endif
+        static void Reverse(int[] array, int start, int end)
+        {
+            while (start < end)
+            {
+                Swap(array, start, end);
+                start++;
+                end--;
+            }
+        }
+    }
+
+}
---
+++ src/Universe.SqlServerJam.Tests/VersionComparerTests.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Universe.SqlServerJam.Tests
+{
+    public class VersionComparerTests
+    {
+        private static readonly int MaxTestPermutations = 100000;
+        string[] Versions => "PG$11.22, PG$11.1, PG$10.1, PG$9.6, PG$9.3, PG$10.3, PG$11.1:7, PG$11.2, PG$11.2:9, PG$11, PG$11.14".Split(',').Select(x => x.Trim()).Where(x => x.Length > 0).ToArray();
+        private Row[] Rows => Versions.Select(x => new Row() { VersionRaw = x }).ToArray();
+
+        [Test]
+        public void TestRawString()
+        {
+            var sorted = Rows.OrderBy(x => x.VersionRaw, VersionStringComparer.Instance);
+            Console.WriteLine(DumpSorted(sorted));
+        }
+
+        [Test]
+        public void TestVersionString()
+        {
+            var sorted = Rows.OrderBy(x => x.VersionSortable);
+            Console.WriteLine(DumpSorted(sorted));
+        }
+
+        [Test]
+        [TestCase("First")]
+        [TestCase("Next")]
+        public void TestAllRawString(string id)
+        {
+            var originalRows = Rows;
+            string expected = null;
+            int index = 0;
+            foreach (var permutation in SimplePermutations.GetAll(originalRows.Length))
+            {
+                // Console.WriteLine(index);
+                var rows = permutation.Select(x => originalRows[x]);
+                var sorted = rows.OrderBy(x => x.VersionRaw, VersionStringComparer.Instance);
+                var actual = DumpSorted(sorted);
+                expected = expected ?? actual;
+                Assert.AreEqual(expected, actual);
+                if (index >= MaxTestPermutations) break;
+                index++;
+            }
+        }
+
+        [Test]
+        [TestCase("First")]
+        [TestCase("Next")]
+        public void TestAllVersionString(string id)
+        {
+            var originalRows = Rows;
+            string expected = null;
+            int index = 0;
+            foreach (var permutation in SimplePermutations.GetAll(originalRows.Length))
+            {
+                var rows = permutation.Select(x => originalRows[x]);
+                var sorted = rows.OrderBy(x => x.VersionSortable);
+                var actual = DumpSorted(sorted);
+                expected = expected ?? actual;
+                Assert.AreEqual(expected, actual);
+                if (index >= MaxTestPermutations) break;
+                index++;
+            }
+
+            Console.WriteLine(expected);
+        }
+
+        private string DumpSorted(IEnumerable<Row> sorted)
+        {
+            var asTextList = sorted.Select(x => $"  * {x.VersionRaw}");
+            var ret = $"Sorted:{Environment.NewLine}{string.Join(Environment.NewLine, asTextList.ToArray())}";
+            // Console.WriteLine(ret);
+            return ret;
+        }
+
+        class Row
+        {
+            public string VersionRaw { get; set; }
+            public VersionString VersionSortable => new VersionString(VersionRaw);
+        }
+
+        [Test]
+        [TestCase(0)]
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(3)]
+        [TestCase(4)]
+        // [TestCase(11)]
+        public void TestPermutations(int count)
+        {
+            
+            Console.WriteLine($"TOTAL: {SimplePermutations.GetAll(count).Count()}");
+            foreach (var permutation in SimplePermutations.GetAll(count))
+            {
+                Console.WriteLine(string.Join(",", permutation.Select(x => x.ToString()).ToArray()));
+            }
+        }
+
+    }
+}
