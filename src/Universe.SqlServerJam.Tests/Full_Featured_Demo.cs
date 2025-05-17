5d2023de5325cd23bd536d14a7bfd0dbbb8d18c5
devizer <devizer@github.io>
2025-05-17 22:38:00 +0300

    Configuration.Affinity with tests on developer edition x86 and x64. Plus Digest

--- src/Universe.SqlServerJam.Tests/Full_Featured_Demo.cs
+++ src/Universe.SqlServerJam.Tests/Full_Featured_Demo.cs
@@ -135,6 +135,7 @@ namespace Universe.SqlServerJam.Tests
 
                     report.AppendLine("Host Platform ............: " + man.HostPlatform);
                     report.AppendLine("CPU Count ................: " + (man.CpuCount == 0 ? "" : man.CpuCount.ToString("n0")));
+                    report.AppendLine("Affinity Cores ...........: " + man.Configuration.AffinityCount);
                     report.AppendLine("Physical Memory (MB) .....: " + (man.PhysicalMemoryKb == 0 ? "" : (man.PhysicalMemoryKb / 1024).ToString("n0")));
                     var availableMemoryKb = man.AvailableMemoryKb;
                     report.AppendLine("Available Memory (MB) ....: " + (availableMemoryKb == 0 ? "" : (availableMemoryKb / 1024).ToString("n0")));
