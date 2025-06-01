9dd82432e76ea1329d7a5f08d484b2b826391a60
devizer <devizer@github.io>
2025-06-01 17:17:06 +0300

    WindowsCpuClockReader.Default tested over sleep and hibernation

---
+++ Sub-Projects/Universe.CpuInfo/Universe.CpuInfo.CLI/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using Universe.CpuInfo;
+
+
+if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+{
+    Console.WriteLine("CPU Frequency for Windows Only");
+    return;
+}
+
+var noDelay = args.Contains("--no-delay", StringComparer.OrdinalIgnoreCase);
+
+Console.WriteLine("CPU Frequency for Windows");
+var errors = 0;
+Stopwatch startAt = Stopwatch.StartNew();
+int? min = null, max = null;
+while (true)
+{
+    var speed = WindowsCpuClockReader.Default.GetSpeed();
+    errors += speed == null ? 1 : 0;
+    if (speed != null && speed.Current > 0) min = min == null ? speed.Current : Math.Min(min.Value, speed.Current);
+    if (speed != null && speed.Current > 0) max = max == null ? speed.Current : Math.Max(max.Value, speed.Current);
+    Console.Write($"{startAt.Elapsed.ToString(@"hh\:mm\:ss")}  {speed}  MIN={min:n0}  MAX={max:n0}  error(s)={errors}  \r");
+    if (!noDelay) Thread.Sleep(1);
+}
+
--- Sub-Projects/Universe.CpuInfo/backup.cmd
+++
@@ -1,5 +0,0 @@
-set FMT=yyyy-MM-dd,HH-mm-ss&& for /f "delims=;" %%i in ('powershell -command "[System.DateTime]::Now.ToString($ENV:FMT)"') DO set datetime=%%i
-
-7za a -t7z -mx=9 -mfb=128 -md=128m -ms=on -mqs=on -xr!.git -xr!bin -xr!obj -xr!.vs ^
-  "C:\Cloud\vg\backup\Universe.CpuInfo (%datetime%).7z" .
-
