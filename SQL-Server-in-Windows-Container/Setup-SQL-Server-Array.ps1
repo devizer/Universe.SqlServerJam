450985ae1821e5cc49e84ea5edcc9ac61f482a10
devizer <devizer@github.io>
2026-01-18 22:11:04 +0200

    Refactoring, from old to new, 4g ram for container, 6 hours, Setup-SQL-Servers-IN-DOCKER.yml [skip ci]

--- SQL-Server-in-Windows-Container/Setup-SQL-Server-Array.ps1
+++ SQL-Server-in-Windows-Container/Setup-SQL-Server-Array.ps1
@@ -77,7 +77,7 @@
         [array]::Reverse($SqlServers)
 
         & curl -o Install-SqlServer-Version-Management.ps1 -kfsSL "https://raw.githubusercontent.com/devizer/Universe.SqlServerJam/master/SqlServer-Version-Management/Install-SqlServer-Version-Management.ps1"
-        foreach($vc in "vcredist2005_x64.exe", "vcredist2005_x86.exe" "vcredist2008_x86.exe", "vcredist2008_x86.exe") {
+        foreach($vc in "vcredist2005_x64.exe", "vcredist2005_x86.exe", "vcredist2008_x86.exe", "vcredist2008_x86.exe") {
           $vcUrl="https://github.com/devizer/glist/blob/master/bin/vcredist/$vc"
           Download-File-FailFree-and-Cached "$vc" "$vcUrl"
         }
