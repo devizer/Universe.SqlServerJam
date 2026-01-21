9729d28ff0084c94bba890886741563a07eae69e
devizer <devizer@github.io>
2026-01-21 09:02:57 +0200

    Big refactoring for Setup SQL [skip ci]

--- SQL-Server-in-Windows-Container/Setup-SQL-Server-Array.ps1
+++ SQL-Server-in-Windows-Container/Setup-SQL-Server-Array.ps1
@@ -95,7 +95,7 @@
           $mnt="type=bind,source=$(Get-Location),target=C:\App"
           echo "--mount parameter is: [$mnt]"
           & docker run -d --name sql-server --memory 4g --cpus 3 "--isolation=$ENV:ISOLATION" --mount "$mnt" --mount type=bind,source=C:\SQL,target=C:\SQL -e SQL="$sql" -e PS1_TROUBLE_SHOOT="On" -e SQLSERVERS_SETUP_FOLDER="C:\Temp\SQL-Setup" `
-            --workdir=C:\App --entrypoint powershell "$($env:THEIMAGE):$($env:TAG)" -Command "Sleep 3600000;"
+            --workdir=C:\App --entrypoint powershell "$($env:THEIMAGE):$($env:TAG)" -Command "Sleep 2147482; Wait-Event;"
 
           & docker exec sql-server powershell -Command "Write-Host STARTING ... ; cd C:\App; Write-Host STRTING at (Get-Location) ...; ls | ft; Write-Host GOING...; . .\Setup-SQL-Server-in-Container.ps1;" |
             tee-object "$ENV:SYSTEM_ARTIFACTSDIRECTORY/OUTPUT $sql.txt"
