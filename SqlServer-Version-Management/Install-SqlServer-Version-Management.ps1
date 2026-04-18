7ffbe6bab22deaf7e539d6357c53be0c8bbe4b7c
devizer <devizer@users.github.io>
2026-04-18 21:58:33 +0300

    Module Setup-SqlServers: Skip Download if service exists [skip ci]

--- SqlServer-Version-Management/Install-SqlServer-Version-Management.ps1
+++ SqlServer-Version-Management/Install-SqlServer-Version-Management.ps1
@@ -4,7 +4,7 @@ Param(
 )
 
 $ModuleName = 'SqlServer-Version-Management';
-$ModuleVersion = '2.1.211';
+$ModuleVersion = '2.1.212';
 $ModuleFiles = @(
 	@{
 		FileName = 'SqlServer-Version-Management\SqlServer-Version-Management.psd1';
@@ -12,10 +12,10 @@ $ModuleFiles = @(
 			"@{",
 			"",
 			"  # RootModule = 'SqlServer-Version-Management.psm1'",
-			"  ModuleToProcess = @('DevOps.psm1')",
+			"  ModuleToProcess = @('SqlServer-Version-Management.psm1')",
 			"",
 			"  # Version below is automatically incremented on build",
-			"  ModuleVersion = `"2.1.211`"",
+			"  ModuleVersion = `"2.1.212`"",
 			"",
 			"  GUID = 'dd03b53d-575a-4056-ae08-e6dfea3384ea'",
 			"",
